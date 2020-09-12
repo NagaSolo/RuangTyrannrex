355ca1afa06697dddc6434f4a2fa741013f89080
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-09-12 13:48:12 +0100

    added; flask server scaffold

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<p>Tyrannrex API</p>"
+
+if __name__ == "__main__":
+    app.run()
