f4c4dcc3248fbd0277b8ef056a67cef95415c4e3
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-09-14 04:51:12 +0100

    updated; json file for data

--- ruangtyrannrex/src/components/Utility/Crafts.js
+++ ruangtyrannrex/src/components/Utility/Crafts.js
@@ -1,32 +1,32 @@
-import ConeIcon from ""
-import MorseIcon from ""
+import CarvanIcon from ""
+import CoinFlipIcon from ""
 
 const Crafts = [
     {
         avatar: "CARVAN",
         title: "Carvan",
         subheader: "September 10th, 2020",
-        titleMedia: "Cone Volume",
-        image: ConeIcon,
-        contentSummary: "Calculator to calculate cone's volume",
-        origin: "Based on Edabit Python Easy challenge",
-        overview: "Calculate cone's volume by providing height and radius of the cone",
-        detail: "Formula to calculate is (radius x radius x pi x height) / 3. This app is a specialized calculator. Click on button ConeVolume to use the app",
-        future: "Next iteration is to produce cone graphic based on the height and radius given",
-        appButton: 'Cone Volume'
+        titleMedia: "",
+        image: CarvanIcon,
+        contentSummary: "Count cars with maximum speed",
+        origin: "Based on Codechef DSA Learning Series",
+        overview: "Count cars with maximum speed on a corner of the road",
+        detail: "Cars can not move with maximum speed is front car is slower. This app is a specialized case. Click on button 'Carvan' to use the app",
+        future: "Next iteration is to produce graphic simulation based on parameters given",
+        appButton: 'Carvan'
     },
     {
         avatar: "COINFLIP",
         title: "Coinflip",
         subheader: "September 09th, 2020",
-        titleMedia: "Morse Code",
-        image: MorseIcon,
-        contentSummary: "Turn your words into Morse code",
-        origin: "Based on Edabit Python Hard challenge",
-        overview: "Convert words from alphabetical letters to Morse Code",
-        detail: "Morse codes combined '.' and '_' to form a letter. This iteration is only capable in translating letter per letter to the Morse codes. Click on 'Encode Morse' button to start the application",
-        future: "Next iteration is to produced corresponding sounds based on the translation",
-        appButton: "Encode Morse"
+        titleMedia: "Coins",
+        image: CoinFlipIcon,
+        contentSummary: "Head Tail flipping game",
+        origin: "Based on Codechef DSA Learning Series",
+        overview: "Head and tail game with predetermined conditions",
+        detail: "Based on conditions winner can be determined. Click on 'Encode Morse' button to start the application",
+        future: "Next iteration is to produce coin flipping simulations",
+        appButton: "Coinflip"
     },
 ]
 
