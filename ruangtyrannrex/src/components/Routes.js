c1fa3c4e20c44649ac39049c8394805d71f66fc4
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-09-21 04:48:46 +0100

    added; ROutes component

---
+++ ruangtyrannrex/src/components/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import {Routes} from 'react-router-dom'
+import { Route } from 'react-router-dom/cjs/react-router-dom.min'
+
+export default function Routes() {
+    return(
+        <React.Fragment>
+            <Route></Route>
+        </React.Fragment>
+    );
+}
