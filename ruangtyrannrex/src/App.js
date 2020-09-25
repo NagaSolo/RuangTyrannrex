514cd5dc5df2e5bca2df19d58b21d32ea52afecb
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-09-25 10:23:21 +0100

    updated; successfully fetching from Flask API to React frontend

--- ruangtyrannrex/src/App.js
+++ ruangtyrannrex/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Login from './components/Login/Login'
+import React from 'react'
+// import Login from './components/Login/Login'
+import Apifetch from './components/Experimental/Apifetch'
 
 function App() {
   return (
     <div>
-      <Login />
+      <Apifetch />
     </div>
   );
 }
--- ruangtyrannrex/src/components/Experimental/Apifetch.js
+++ ruangtyrannrex/src/components/Experimental/Apifetch.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react';
 
 export default function Apifetch() {
-  const [currentHome, setCurrentHome] = useState('Cobaan');
+  const [currentHome, setCurrentHome] = useState('Testing');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api').then(res => res.json()).then(data => {
-      setCurrentHome(data.api);
+    fetch('/lapindromes').then(res => res.json()).then(data => {
+      setCurrentHome(data.Answer);
     });
   }, []);
   return (
