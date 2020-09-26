3f521f531ec04cd9343e1b965749fbc9b520f3cd
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-09-26 09:17:33 +0100

    renamed; Apifetch component to the Lapindromes

--- ruangtyrannrex/src/components/Experimental/Lapindromes.js
+++ ruangtyrannrex/src/components/Experimental/Lapindromes.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
-export default function Apifetch() {
+export default function Lapindromes() {
   const [currentHome, setCurrentHome] = useState('Testing');
 
   useEffect(() => {
