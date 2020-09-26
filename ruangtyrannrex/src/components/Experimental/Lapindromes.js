39d1d4ae0e527e866a607e81d4195f8ee7903cf1
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-09-26 10:17:39 +0100

    modification; to useEffect for button click

--- ruangtyrannrex/src/components/Experimental/Lapindromes.js
+++ ruangtyrannrex/src/components/Experimental/Lapindromes.js
@@ -1,18 +1,31 @@
 import React, {useState, useEffect} from 'react';
 
 export default function Lapindromes() {
-  const [currentHome, setCurrentHome] = useState('Testing');
 
-  useEffect(() => {
-    fetch('/lapindromes').then(res => res.json()).then(data => {
-      setCurrentHome(data.Answer);
-    });
-  }, []);
+  let [currentInput, setCurrentInput] = useState('Enter your input ..')
+
+  let [currentAnswer, setCurrentAnswer] = useState('Testing');
+  
+  const handleClick = () => {
+    useEffect(() => {
+      fetch('/lapindromes').then(res => res.json()).then(data => {
+        setCurrentAnswer(data.Answer);
+      });
+    }, []);
+  }
   return (
     <div>
-      <header>
-        {currentHome}
-      </header>
+      <input 
+        type='text'
+        value={currentInput}
+        onChange={event => setCurrentInput(event.target.value)}
+      />
+      <button onClick={handleClick}>Send</button>
+      <div>
+        <h3>It is.. 
+          {currentAnswer}
+        </h3>
+      </div>
     </div>
   );
 }
