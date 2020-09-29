a14731bb8c2048aeec38b8307e04bc01f03ba419
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-09-29 12:47:20 +0100

    added; viewing results from flask api using render_templates

--- ruangtyrannrex-flask/app.py
+++ ruangtyrannrex-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 from modules.lapindromes import palindrome
@@ -10,7 +10,9 @@ def home():
 @app.route('/lapindromes', methods=['GET', 'POST'])
 def lapindromes():
     if request.method == 'POST':
-        the_string = request.values.get()
-        return {'Answer': palindrome(the_string)}
+        the_string = request.form["the_input"]
+        return render_template('lapindromes.html', answer=palindrome(the_string))
+        # return {'Answer': palindrome(the_string)}
     else:
-        return f'Value is unspecified'
+        # return f'Value is unspecified'
+        return render_template('lapindromes.html')
