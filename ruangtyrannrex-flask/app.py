ae2a7a6edba7a079cbddcc69e71df8904735dbf6
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-10-03 16:53:03 +0100

    modified; input for zco14003, integration with app.py

--- ruangtyrannrex-flask/app.py
+++ ruangtyrannrex-flask/app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 from modules.lapindromes import palindrome
 from modules.fortytwo import fortytwo
 from modules.flow007 import reversal
+from modules.zco14003 import max_profit_from_budget
 
 @app.route('/')
 def home():
@@ -32,6 +33,13 @@ def flow_007():
         the_string = request.form["the_number"]
         return render_template('flow007.html', answer=f'Reversed form is {reversal(the_string)}')
     return render_template('flow007.html')
+
+@app.route('/zco14003', methods=['GET', 'POST'])
+def opt_prices():
+    if request.method == 'POST':
+        the_string = request.form["the_budget"]
+        return render_template('zco14003.html', answer=f'Reversed form is {max_profit_from_budget(the_string)}')
+    return render_template('zco14003.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
