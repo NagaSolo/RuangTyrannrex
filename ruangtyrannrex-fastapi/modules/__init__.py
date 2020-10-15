71bb8ffb49585365105f81e9dcf73451f38f817a
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2020-10-15 08:17:32 +0100

    updated; __init__ module, entrypoint

--- ruangtyrannrex-fastapi/modules/__init__.py
+++ ruangtyrannrex-fastapi/modules/__init__.py
@@ -1,10 +1,2 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
-
-# app.mount('/static', StaticFiles(directory='./modules/static', name='static'))
-templates = Jinja2Templates(directory='/modules/templates')
-
-from modules.views import main
+from modules import carvan, coinflip, fctrl, flow007, fortytwo, laddu, lapindromes, multhree, zco14003
